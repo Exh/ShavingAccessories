ad41e39881b6e1ba131c4a90f438c84220b29e15
e.tarasenko <exh@xakep.ru>
2017-04-09 15:49:31 +0300

    checking unit tests commit

---
+++ project/tests/service-test.js
@@ -0,0 +1,15 @@
+"use strick"
+
+var assert = require('chai').assert;
+
+suite('Check unit system', function () {
+
+    setup(function () {
+
+    });
+
+    test('Check test', function(){
+        assert.equal(7, 8);
+    });
+
+});
