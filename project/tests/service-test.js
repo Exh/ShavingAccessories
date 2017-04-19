948c3d992ab4a0276e2b0f0469c771839641a7d4
e.tarasenko <exh@xakep.ru>
2017-04-19 23:53:03 +0300

    update test file

--- project/tests/service-test.js
+++ project/tests/service-test.js
@@ -1,9 +1,9 @@
 "use strick"
 
 var assert = require('chai').assert;
-var Subscribing = require('../js/subscribing');
-var User = require('../js/user');
-var Product = require('../js/product');
+var Subscribing = require('../js/domain/subscribing');
+var User = require('../js/domain/user');
+var Product = require('../js/domain/product');
 
 suite('Shaving accessories subscribing', function () {
     suite('As User I can', function () {
@@ -28,6 +28,17 @@ suite('Shaving accessories subscribing', function () {
 
             let res = user.subscribing.product.title;
             assert.equal(res, "Shaver + Shaving gel")
+        });
+
+        test('to choose shaver + shaving gel + aftershaves as the product of subscribe', function () {
+            let user = new User();
+            let product = new Product("Shaver + Shaving gel + Aftershaves");
+            let subscribing = new Subscribing(product);
+
+            user.setSubscribing(subscribing);
+
+            let res = user.subscribing.product.title;
+            assert.equal(res, "Shaver + Shaving gel + Aftershaves")
         });
 
     });
