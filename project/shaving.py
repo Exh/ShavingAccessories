607aee837e56495d72980df99a9adf5c36d1cc2f
Evgeny Tarasenko <e.tarasenko@octavianonline.com>
2017-04-20 10:19:57 +0300

    add ProductBuilder, add price property to tproduct

--- project/shaving.py
+++ project/shaving.py
@@ -26,8 +26,9 @@ class Subscribing(object):
 
 
 class Product(object):
-	def __init__(self):
-		self._title = "Shave"
+	def __init__(self, title, price):
+		self._title = title
+		self._price = price
 
 	@property
 	def title(self):
@@ -36,3 +37,28 @@ class Product(object):
 	@title.setter
 	def title(self, v):
 		self._title = v
+
+
+	@property
+	def price(self):
+		return self._price
+
+	@price.setter
+	def price(self, v):
+		self._price = v
+
+class ProductBuilder(object):
+	def __init__(self):
+		self._title = ""
+		self._price = 0
+
+	def create(self):
+		return Product(self._title, self._price)
+
+	def withTitle(self, title):
+		self._title = age
+		return self
+
+	def withPrice(self, price):
+		self._price = price
+		return self
